b65bef3e00169572fba82ad0b628d4a0061d97b8
imagham <151702624+imagham@users.noreply.github.com>
2024-10-03 09:21:28 +0700

    Ujian STS Fadgham

--- UTS/exam.mjs
+++ UTS/exam.mjs
@@ -14,9 +14,9 @@ function getDivisibleByFourString2() {
       if (a % 2 == 1) {
         console.log("angka ganjil");
       } else if (a % 4 == 0) {
-        console.log("angka genap, bisa dibagi 4");
+        console.log("angka genap,dan bisa dibagi 4");
       } else if (a % 2 == 0) {
-        console.log("angka genap");
+        console.log("angka genap,dan tidak bisa dibagi 4");
       }
     }
   }
